5b7013d43f36c2879516fd10405ea0723942d84a
hacke2 <hacke2cn@gmail.com>
2023-12-21 14:48:40 +0800

    Update index.zh.tsx

--- packages/site/site/pages/index.zh.tsx
+++ packages/site/site/pages/index.zh.tsx
@@ -105,7 +105,7 @@ const IndexPage: React.FC = () => {
         logoUrl={logoUrl}
         title={t('什么是 Codeblitz')}
         description={t(
-          'Codeblitze 是由蚂蚁云研发团队打造的基于 OpenSumi 的纯前端 IDE 基础框架，相比于我们传统的 Cloud IDE，最大的特点是无需容器，只需一个浏览器就能运行 Web IDE。在保证体验一致的基础之上具备启动快，定制灵活、接入方便等特点',
+          'Codeblitz 是由蚂蚁云研发团队打造的基于 OpenSumi 的纯前端 IDE 基础框架，相比于我们传统的 Cloud IDE，最大的特点是无需容器，只需一个浏览器就能运行 Web IDE。在保证体验一致的基础之上具备启动快，定制灵活、接入方便等特点',
         )}
         className="banner"
         buttons={bannerButtons}
